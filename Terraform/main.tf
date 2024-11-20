1f1baea68c8abfc0e70b71ca23debf3088fe069e
himani211 <himanihingve1@gmail.com>
2024-11-20 12:29:34 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -61,7 +61,7 @@ resource "azurerm_network_interface" "nic_linux" {
 
   ip_configuration {
     name                          = "ipconfig-linux-S3"
-    subnet_id                     = azurerm_subnet.subnet.id
+    subnet_id                     = azurerm_subnet.subnet11.id
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.vm_public_ip11.id
   }
